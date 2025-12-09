799c35b4c296f17e20db1cfa323e7e50cf371583
Torto0ga-B <luca.wilson24@bathspa.ac.uk>
2025-12-09 04:35:17 +0000

    Training split
    Worked on the code for the training, validation testing split and with the print statement to test to see if it partitioned the data out accordingly, i'm glad to say it did

--- train.py
+++ train.py
@@ -8,11 +8,26 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 # Load and prepare the data
 
-df = pd.read_csv("data/Telco_Customer_Churn.csv") # Load csv into dataframe
+df = pd.read_csv("data\Telco_Cusomer_Churn.csv") # Load csv into dataframe
 df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1}) # Maps the Churn column to binary integers as ML models need numeric targets
 
 x = df.drop("Churn", axis=1)
 y = df["Churn"]
 
 
+# Train / validation / Test
 
+# First split of training against the temp data
+
+x_train, x_temp, y_train, y_temp = train_test_split(
+    # 30% of the original dataset goes into the test size and you're left with 70%
+    x, y, test_size = 0.30, stratify = y, random_state = 42
+)
+
+# Second split of data
+x_val, x_test, y_val, y_test = train_test_split(
+    # From that 30% it's halfed leaving 15% for each
+    x_temp, y_temp, test_size = 0.50, stratify = y_temp, random_state = 42
+)
+
+print("Train:", x_train.shape, "Val:", x_val.shape, "Test:", x_test.shape)
