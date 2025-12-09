e9c2395ecfd8600bd9d4b50b35edded9b0c93e03
Torto0ga-B <luca.wilson24@bathspa.ac.uk>
2025-12-09 01:23:54 +0000

    Initialise preprocessing
    Get all the imports needed for the pre-processing code

---
+++ src/preprocess.py
@@ -0,0 +1,5 @@
+from sklearn.pipeline import Pipeline # Chains multiple transformations and a model together 
+from sklearn.compose import ColumnTransformer # Allows for different preprocessing to different features
+from sklearn.impute import SimpleImputer # Fills missing values
+from sklearn.preprocessing import OneHotEncoder, StandardScaler # OHE converts categorical variables to numerical format for ML, SS does Z-score normalisation
+
