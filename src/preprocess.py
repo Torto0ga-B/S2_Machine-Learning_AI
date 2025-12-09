a229871800ccb095ac684601e5056969c33dd2ad
Torto0ga-B <luca.wilson24@bathspa.ac.uk>
2025-12-09 01:26:15 +0000

    Separate columns by type
    Code to separate the columns by type either categorical or numerical as the numerical can undergo z-score normalisation but categorical can't and needs to be processed

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,3 +3,6 @@ from sklearn.compose import ColumnTransformer # Allows for different preprocessi
 from sklearn.impute import SimpleImputer # Fills missing values
 from sklearn.preprocessing import OneHotEncoder, StandardScaler # OHE converts categorical variables to numerical format for ML, SS does Z-score normalisation
 
+num_cols = [...] # list of numerical features e.g. tenure, MonthlyCharges
+cat_cols = [...] # list of categorical features e.g. gender, contract
+
