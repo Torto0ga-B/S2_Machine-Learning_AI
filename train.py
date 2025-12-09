d740a4cc59e4b25c4f1f6e5b287dc8bd9875d1c8
Torto0ga-B <luca.wilson24@bathspa.ac.uk>
2025-12-09 04:19:05 +0000

    Load the data
    Coded the loading of the csv to the dataframe and mapping it to binary integers and coding the separate features of x and the target y

--- train.py
+++ train.py
@@ -6,3 +6,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
+# Load and prepare the data
+
+df = pd.read_csv("data/Telco_Customer_Churn.csv") # Load csv into dataframe
+df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1}) # Maps the Churn column to binary integers as ML models need numeric targets
+
+x = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+
+
